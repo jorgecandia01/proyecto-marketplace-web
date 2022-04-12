e93758d36e9831a8c2997e507cce3ef729ba22f4
paulJdN <paul.jouslin@gmail.com>
2022-04-12 19:37:08 +0200

    Update con todo el frontend y contacto que fonctiona con controller

---
+++ demo/src/main/java/com/example/demo/Controller/ContactController.java
@@ -0,0 +1,33 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ContactController {
+
+    @PostMapping("save-email")
+    @ResponseBody
+    public String sendEmail(@RequestParam (name="name") String name, 
+                            @RequestParam (name="email") String email, 
+                            @RequestParam (name="phone") String phone,
+                            @RequestParam (name="message") String message){
+
+        return "<html>"+
+                    "<head> <title= 'Acciona - Respuesta contacto'><head>"+
+                    "<body align='center' style='background-color:grey'>"+
+                        "<h3> MENSAJE ENVIADO </h3>"+
+                        "<p> Muchas gracias por el mensaje, te contestamos dentro de una semana</p>"+
+                        "<p> Aqui un recapitulo</p>"+
+                        "<p> Name : "+ name+"</p>"+
+                        "<p> Email : "+ email+"</p>"+
+                        "<p> Telefono : "+ phone+"</p>"+
+                        "<p> Mensaje : "+ message+"</p>"+
+                        "<button> <a href='index.html'>Volver al inicio </a></button>"+
+                    "</body>"+
+                "</html>";
+    }
+    
+}
